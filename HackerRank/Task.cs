6eb0939b1ffd1ef7998e2271382d4886eb73bba3
MasterBass <vasya_mail@ukr.net>
2018-02-25 11:50:28 +0200

    initial commit

---
+++ HackerRank/Task.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HackerRank
+{
+    public class Task
+    {
+        public int getNumber(string[] origItems,float[] origPrices, string[] items, float[] prices)
+        {
+            int result = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (origPrices[Array.IndexOf(origItems, items[i])] != prices[i])
+                    result++;
+            }
+            return result;
+        }
+
+        [Test]
+        public void getNumberTest1()
+        {
+            var origItems = new [] {"rice", "sugar", "wheat", "cheese"};
+            var origPrices = new [] {16.89f, 56.92f, 20.89f, 345.99f};
+            var items = new [] {"rice", "cheese"};
+            var prices = new [] {10.89f, 340.99f};
+            Assert.AreEqual(getNumber(origItems, origPrices, items, prices), 2);
+        }
+        
+        
+        [Test]
+        public void getNumberTest2()
+        {
+            var origItems = new [] {"rice", "sugar", "wheat", "cheese"};
+            var origPrices = new [] {16.89f, 56.92f, 20.89f, 345.99f};
+            var items = new [] {"rice", "cheese"};
+            var prices = new [] {16.89f, 340.99f};
+            Assert.AreEqual(getNumber(origItems, origPrices, items, prices), 1);
+        }
+    }
+}
